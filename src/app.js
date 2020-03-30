500ee310fc0a8085330fa069c4e901b4175a2af8
Kevin Vizconde <kcarlv@gmail.com>
2020-03-29 22:28:30 -0700

    Kevin (#11)
    * initial commit, setting up the paths back-end to front-end
    
    * Kevin (#3)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted kevin.txt
    
    * Kevin (#4)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted gitignore on my branch
    
    * delete kevin.txt
    
    * updated gitignore
    
    * first task
    
    * updated html/css
    
    * updated queries
    
    * finished html/css
    
    * .
    
    * beautified html
    
    * changed styles.css to style.css
    
    * updated css and html, added blob effect
    
    * updated index turnedd into hbs file and also completed registration page

--- src/app.js
+++ src/app.js
@@ -15,13 +15,52 @@ app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
+/* --HandleBar Helpers-- */
+
+// Container For Contents
+hbs.registerHelper('container', options => {
+  return new hbs.SafeString(
+    `<main class="welcome-container">
+    ${options.fn(this)}
+    </main>`,
+  );
+});
+
+// Container for Header Login/Registration
+hbs.registerHelper('header_login', options => {
+  return new hbs.SafeString(
+    `<section class="header-image-container">
+    <div class="header">
+    ${options.fn(this)}
+    </div>
+    </section>`,
+  );
+});
+
+// Container for Login Forms
+hbs.registerHelper('forms', options => {
+  return new hbs.SafeString(
+    `<section class="login-container">
+    ${options.fn(this)}
+    </section>`,
+  );
+});
+
+/* --END-HandleBar Helpers-- */
+
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 // MATT & BENSON your back-end stuff starts here
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Knowledge Base',
+    title: 'Where Developers Learn, Share & Refactor',
+  });
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', {
+    title: 'Registration Page',
   });
 });
 

b5437f208528051bd9f524732be030487d1beb5a
Benson <bensonwang015@gmail.com>
2020-04-03 16:31:18 -0700

    Database files

---
+++ database/userDataFile.js
@@ -0,0 +1,21 @@
+let datab = require('../util/database');
+
+function addUsers(userInfo) {
+    let sql = "Insert into users(firstname, lastname, email, password, passwordconfirm) values ('" + userInfo.firstname + "','"+ userInfo.lastname+ "','" + "','"+ userInfo.about+ "','" + userInfo.email + "','"+ userInfo.password+ "','" + "','"+ userInfo.passwordconfirm + "','" + "')";
+    
+    datab.execute(sql);
+}
+
+function getAllExistingUsers() {
+    return datab.execute('Select * from users');
+}
+
+function getSpecificUser(id) {
+    return datab.execute("Select * from users where id = " + id);
+}
+
+module.exports = {
+    add : addUsers,
+    getall : getAllExistingUsers,
+    getpeople: getSpecificUser 
+}

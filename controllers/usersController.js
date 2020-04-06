a31aa79e8e98f31d558f4e20b3e5082569634a8a
Kevin Vizconde <kcarlv@gmail.com>
2020-04-05 21:32:57 -0700

    fixing conflicts

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-let userModel = require('../models/userDataFile');
-
-exports.getAllExistingUsers = (req, res, next) => {
-    let allUsers = userModel.getall();
-    
-    allUsers.then(([rows, fieldData]) => {
-       res.render('users', {style: rows, style: true}); 
-    });
-}
-
-exports.getAddUsers = (req, res, next) => {
-    res.render('register', {formCSS: true});
-};
-
-exports.getUsers = (req, res, next) => {
-    let id = req.params.id;
-    let users = userModel.getUsers(id);
-    users.then( ([data, metadeta]) => {
-        res.render('home', {home: data[0], style: true});
-    });
-}
-
-exports.postAddUsers = (req, res, next) => {
-    let u_firstname = req.body.firstname;
-    let u_lastname = req.body.lastname;
-    let u_email = req.body.email;
-    let u_password = req.body.password;
-    let u_passwordconfirm = req.body.passwordconfirm;
-    
-    let c0dingProject = {
-        firstname: u_firstname,
-        lastname: u_lastname,
-        email: u_email,
-        password: u_password,
-        passwordconfirm: u_passwordconfirm
-    }
-    
-    userModel.add(c0dingProject);
-    res.redirect(301, '/usersRoutes');
-}
-=======
 const userModel = require('../models/userDataFile');
 
 exports.getAllExistingUsers = (req, res, next) => {
@@ -87,4 +45,3 @@ exports.postAddUsers = (req, res, next) => {
   userModel.add(c0dingProject);
   res.redirect(301, '/usersRoutes');
 };
->>>>>>> 0b20d14a06fc643401675c4b19ae0df6bbcad1a3

b90a5a3ab9b08e477408a27c21e0904169d7b8a6
Kevin Vizconde <kcarlv@gmail.com>
2020-04-06 17:03:51 -0700

    Kevin (#42)
    * initial commit, setting up the paths back-end to front-end
    
    * Kevin (#3)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted kevin.txt
    
    * Kevin (#4)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted gitignore on my branch
    
    * delete kevin.txt
    
    * updated gitignore
    
    * first task
    
    * updated html/css
    
    * updated queries
    
    * finished html/css
    
    * .
    
    * beautified html
    
    * changed styles.css to style.css
    
    * updated css and html, added blob effect
    
    * updated index turnedd into hbs file and also completed registration page
    
    * updated css
    
    * fixed css
    
    * added navbar partial
    
    * deleted extra partials, combined wthem to navbar_main
    
    * updated main page
    
    * more updates on home
    
    * home page finished for front end
    
    * finished home final for front end
    
    * updated css
    
    * updated head partials
    
    * fixed up folder structure and app.js routes
    
    * updated css, added profile page
    
    * added previous bttn
    
    * added message.hbs and adjusted css accordingly
    
    * updated message.hbs
    
    * fixed app.js
    
    * fixing conflicts
    
    * commented out mysql stuff
    
    * added rowtoggle function
    
    * changed functions to arrow functions on app.js to comply with eslint airbnb standards
    
    * fixed occupation css class, and updated profile_info for image url
    
    * updated index.js, also formatted controllers, and database and partials

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'usersDataStorage',
-    password: 'COMP-4711'
+  host: 'localhost',
+  user: 'root',
+  database: 'usersDataStorage',
+  password: 'COMP-4711',
 });
 
 module.exports = pool.promise();

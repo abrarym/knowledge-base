898dbade51fbe6651d3f550342dc126c7ca40458
abrarym <mabrary@my.bcit.ca>
2020-04-06 20:34:59 -0700

    changed database and sql calls

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'usersDataStorage',
-    password: 'COMP-4711'
+    database: 'knowledgebase',
+    password: '7S::knowledge'
 });
 
 module.exports = pool.promise();

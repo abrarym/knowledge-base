f8faa15b2e44b23c5b34eb5f998aab12efeb4fc7
Kevin Vizconde <kcarlv@gmail.com>
2020-04-07 00:23:10 -0700

    Kevin (#48)
    * added a feature test file
    
    * added matt's feature/branch
    
    * .
    
    * added benson's feature
    
    * added matt's feature/branch
    
    * added benson's feature
    
    * .
    
    * deleted benson.txt
    
    * del matt.txt
    
    * added a feature test file
    
    * .
    
    * added benson's feature
    
    * added benson's feature
    
    * .
    
    * deleted benson.txt
    
    * added matt's feature/branch
    
    * del matt.txt
    
    * initial commit, setting up the paths back-end to front-end
    
    * Kevin (#3)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted kevin.txt
    
    * Kevin (#4)
    
    * added a feature test file
    
    * added gitignore
    
    * deleted gitignore on my branch
    
    * delete kevin.txt
    
    * updated gitignore
    
    * first task
    
    * updated html/css
    
    * updated queries
    
    * finished html/css
    
    * .
    
    * beautified html
    
    * changed styles.css to style.css
    
    * updated css and html, added blob effect
    
    * updated index turnedd into hbs file and also completed registration page
    
    * updated css
    
    * fixed css
    
    * added navbar partial
    
    * deleted extra partials, combined wthem to navbar_main
    
    * updated main page
    
    * more updates on home
    
    * home page finished for front end
    
    * finished home final for front end
    
    * updated css
    
    * updated head partials
    
    * fixed up folder structure and app.js routes
    
    * Database files
    
    * edited task
    
    * fixed an error with function addUser
    
    * fixed an error with function addUser
    
    * fixed an error with function addUser
    
    * fixed an error with function addUser
    
    * added route and controller
    
    * new stuff
    
    * fixed inconsistencies
    
    * working code
    
    * updated css, added profile page
    
    * added previous bttn
    
    * added message.hbs and adjusted css accordingly
    
    * updated message.hbs
    
    * navigation to home page works
    
    * fixed app.js
    
    * fixing conflicts
    
    * commented out mysql stuff
    
    * added rowtoggle function
    
    * current version should have user data once you get to the home screen and replace most of the data
    
    * fixed some merge issues
    
    * changed functions to arrow functions on app.js to comply with eslint airbnb standards
    
    * fixed occupation css class, and updated profile_info for image url
    
    * updated index.js, also formatted controllers, and database and partials
    
    * deleted _profile avatar partial, and updated profile_posts
    
    * added inbox page
    
    * changed database and sql calls
    
    * updated css, and class names for partials
    
    * updated if condition for sub headings home and profile views
    
    * restructured app usercontroller and userroutes files
    
    * updated usersController
    
    Co-authored-by: Benson <bensonwang015@gmail.com>
    Co-authored-by: Matthew Abrary <43010807+abrarym@users.noreply.github.com>
    Co-authored-by: abrarym <mabrary@my.bcit.ca>

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const path = require('path');
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const db = require('./util/database.js');
-const dataFile = require('./models/userDataFile.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-let userInfo = {};
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, 'public');
@@ -33,70 +31,13 @@ app.use(express.static(publicDirectoryPath));
 // create a variable that links to the route
 const userRoutesFile = require('./routes/usersRoutes');
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'Where Developers Learn, Share & Refactor',
   });
 });
 
 app.use(userRoutesFile);
-
-app.post('', urlencodedParser, (req, res) => {
-  userInfo = Object.assign({}, userInfo, req.body);
-  console.log(userInfo);
-  res.render('register', {
-    title: 'Registration page',
-    data: req.body,
-  });
-});
-
-app.get('/register', urlencodedParser, (req, res) => {
-  console.log(req.body);
-  res.render('register', {
-    title: 'Registration Page',
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
-});
-
-app.post('/register', urlencodedParser, (req, res) => {
-  userInfo = Object.assign({}, userInfo, req.body);
-  console.log(userInfo);
-  dataFile.add(userInfo);
-  res.render('home', {
-    title: 'Home Page',
-    data: userInfo,
-  });
-});
-
-app.get('/home', urlencodedParser, (req, res) => {
-  console.log(req.body);
-  res.render('home', {
-    title: 'Home Page',
-    isProfile: false,
-    isHome: true,
-  });
-});
-
-app.get('/profile', (req, res) => {
-  res.render('profile', {
-    title: 'Profile Page',
-    isProfile: true,
-    isHome: false,
-  });
-});
-
-app.get('/message', (req, res) => {
-  res.render('message', {
-    title: 'Message Page',
-  });
-});
-
-app.get('/inbox', (req, res) => {
-  res.render('inbox', {
-    title: 'Inbox Page',
-  });
-});
 
 app.listen(port, () => {
   console.log('server is up on port 3000');

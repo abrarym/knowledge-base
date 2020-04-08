9f6ff81d490ff2d117682786fb9380f4796b1b6a
abrarym <mabrary@my.bcit.ca>
2020-04-07 17:39:15 -0700

    still modifying the database credentials to work with heroku

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'b8afeaa1472c8c',
     database: 'heroku_38b949a65d7a24b',
     password: '263ecc04'

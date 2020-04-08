9586535736e6962895e559f704f3ca422d5b8bbb
abrarym <mabrary@my.bcit.ca>
2020-04-07 17:15:42 -0700

    had to commit to change the database credentials so that it would be linked to clearDB on heroku

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    database: 'knowledgebase',
-    password: '7S::knowledge'
+    user: 'b8afeaa1472c8c',
+    database: 'heroku_38b949a65d7a24b',
+    password: '263ecc04'
 });
 
 //
